f21b020a92e7592bc194eef601674e2ece6e1133
Alexcarvalho73 <alexcarvalho73@hotmail.com>
2026-05-07 14:42:41 -0400

    Calibracao: ROI Câmera 01 ajustado para área vermelha da esteira principal

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ CAMERAS = {
     "camera_01": {
         "name": "Esteira Principal - Ruptura",
         "rtsp_url": "rtsp://admin:013579ab@10.200.34.50:554/cam/realmonitor?channel=1&subtype=0",
-        "roi": [[340, 480], [1250, 410], [1650, 750], [460, 870]],
+        "roi": [[480, 380], [1240, 345], [1250, 740], [350, 770]],
         "type": "color_detection"
     },
     "camera_02": {
